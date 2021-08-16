ed49ec3f342e2a698ce52c94ff8c04406378fac3
Mihaita Iavorovschi <mihaita.iavorovschi@fortech.ro>
2021-08-16 10:14:37 +0300

    login

--- ToyAppFE/src/main/java/ro/toyapp/frontend/ui/Login.java
+++ ToyAppFE/src/main/java/ro/toyapp/frontend/ui/Login.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
@@ -25,18 +27,15 @@ private ApiCallsUtil apiCallsUtil;
 	
 	
 	public void addForm() {
-		HorizontalLayout formLayout = new HorizontalLayout();
-		formLayout.setWidth("80%");
-		
+		LoginOverlay component = new LoginOverlay();
+		component.addLoginListener(e -> component.close());
+		Button open = new Button("Open login overlay",
+		    e -> component.setOpened(true));
 
-        TextField taskField = new TextField();
-        taskField.focus();
-        
-        PasswordField passwordField= new PasswordField();
-        Button loginButton = new Button("Login");
-        
-        formLayout.add(taskField);
-        formLayout.add(passwordField);
-        formLayout.add(loginButton);
+		LoginI18n i18n = LoginI18n.createDefault();
+		i18n.setAdditionalInformation("To close the login form submit non-empty username and password");
+		component.setI18n(i18n);
+
+		add(component, open);
 	}
 }
